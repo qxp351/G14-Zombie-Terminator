5d03551ace1a19600cec19dd80c56c5209ec1ffc
njmauch <32074626+njmauch@users.noreply.github.com>
2020-11-30 00:12:15 -0600

    Home base, able to move around, added 2 npc's one to add health and one to set up next level.
    Home base, able to move around, added 2 npc's one to add health and one to set up next level.

---
+++ Assets/NextLevelNPC.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
+
+public class NextLevelNPC : MonoBehaviour
+{
+    private bool triggering;
+    public GameObject npcText;
+    public GameObject newLevelText;
+    private bool interacting = false;
+    private LevelConditions levelConditions;
+
+    void Start()
+    {
+        levelConditions = GetComponent<LevelConditions>();
+    }
+    private void Update()
+    {
+        if (triggering)
+        {
+            npcText.SetActive(true);
+        }
+        else
+        {
+            npcText.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.F) && triggering)
+        {
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            interacting = true;
+            newLevelText.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cancel();
+        }
+    }
+    public void Dusk()
+    {
+        levelConditions.timeOfDay = LevelConditions.Time.dusk;
+
+    }
+    public void Day()
+    {
+        levelConditions.timeOfDay = LevelConditions.Time.day;
+
+    }
+    public void Night()
+    {
+        levelConditions.timeOfDay = LevelConditions.Time.night;
+    }
+    public void Docile()
+    {
+        levelConditions.zombieAggression = LevelConditions.Difficulty.docile;
+
+    }
+    public void Agitated()
+    {
+        levelConditions.zombieAggression = LevelConditions.Difficulty.agitated;
+
+    }
+    public void Crazed()
+    {
+        levelConditions.zombieAggression = LevelConditions.Difficulty.crazed;
+
+    }
+
+    public void Go()
+    {
+        //SceneManager.SetActiveScene("INSERT SCENE HERE");
+    }
+    public void Cancel()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        newLevelText.SetActive(false);
+        triggering = false;
+        interacting = false;
+        Time.timeScale = 1f;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            triggering = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            triggering = false;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/NPC_HP.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_HP : MonoBehaviour
+{
+    public GameObject npcText;
+    public GameObject tradeText;
+    private GameObject triggeringNpc;
+    private bool triggering, interacting;
+    private SuppliesManager supplies;
+    private PlayerStats player;
+
+    private void Start()
+    {
+        supplies = GetComponent<SuppliesManager>();
+        player = GetComponent<PlayerStats>();
+    }
+    void Update()
+    {
+        if(triggering)
+        {
+            npcText.SetActive(true);
+        }
+        else
+        {
+            npcText.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.F) && triggering)
+        {
+            triggering = false;
+            tradeText.SetActive(true);
+            interacting = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Y) && interacting)
+        {
+            supplies.UseFood();
+            player.Heal(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.N) && interacting)
+        {
+            interacting = false;
+            tradeText.SetActive(false);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            triggering = true;
+            triggeringNpc = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            triggering = false;
+            triggeringNpc = null;
+            interacting = false;
+        }
+    }
+}
